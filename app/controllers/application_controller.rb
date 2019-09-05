d6c02de293ff3518c96cd1fbfa513a9579da92be
Brandon Espinoza <espinozab100@gmail.com>
2019-09-05 12:50:34 -0500

    Created devise users with custom fields

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :username, :bio, :birthdate, :twitch_username, :mixer_username, :instagram_username, :youtube_username)}
+
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :remember_me)}
+    
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :username, :bio, :birthdate, :twitch_username, :mixer_username, :instagram_username, :youtube_username, :email, :password, :password_confirmation, :remember_me)}
+
+  end
+
 end
